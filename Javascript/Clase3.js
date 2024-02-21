b1bbe9cfecced0bd6f235f17bb694cb81318546d
MAGA1314 <garciaagudelomiguelangel@gmail.com>
2024-02-21 12:43:48 -0500

    clase3_variables

---
+++ Javascript/Clase3.js
@@ -0,0 +1,29 @@
+let nuevoUsuario;
+let user = "Miguel";
+
+console.log(user);
+console.log();
+
+//Operador ternario
+
+user === "Pedro"? console.log("erdadero"): console.log("Falso");
+
+//const es parecido a constante
+const pi = 3.1416;
+//trato de cambiar el valor da error
+// pi = 26
+
+//metodos propios del lenguaje
+let PI_Dos;
+PI_Dos = Math.pi;
+user.includes("P");
+console.log(user.length);
+
+//metodos o funciones
+//arrow function
+const saludarUsuario=() =>{
+    return "Hola Juan";
+}
+const saludarUsuariosaludarUser = user =>{
+    return "Hola", user;
+}
